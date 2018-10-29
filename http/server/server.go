a7722e86c755de2dabe0572d3ede7fc01576da86
lunaczp <moon.zhp@gmail.com>
2018-10-29 16:45:40 +0800

    simple http server

--- http/server/server.go
+++ http/server/server.go
@@ -1,47 +1,17 @@
-//from https://golang.org/doc/effective_go.html#web_server
 package main
 
 import (
-	"flag"
-	"html/template"
-	"log"
+	"fmt"
 	"net/http"
 )
 
-var addr = flag.String("addr", ":1718", "http service address") // Q=17, R=18
-
-var templ = template.Must(template.New("qr").Parse(templateStr))
-
 func main() {
-	flag.Parse()
-	http.Handle("/", http.HandlerFunc(QR))
-	err := http.ListenAndServe(*addr, nil)
-	if err != nil {
-		log.Fatal("ListenAndServe:", err)
-	}
-}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "This is a website server by a Go HTTP server.")
+	})
 
-func QR(w http.ResponseWriter, req *http.Request) {
-	templ.Execute(w, req.FormValue("s"))
-}
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-const templateStr = `
-<html>
-<head>
-<title>QR Link Generator</title>
-</head>
-<body>
-{{if .}}
-<img src="http://chart.apis.google.com/chart?chs=300x300&cht=qr&choe=UTF-8&chl={{.}}" />
-<br>
-{{.}}
-<br>
-<br>
-{{end}}
-<form action="/" name=f method="GET"><input maxLength=1024 size=70
-name=s value="" title="Text to QR Encode"><input type=submit
-value="Show QR" name=qr>
-</form>
-</body>
-</html>
-`
+	http.ListenAndServe(":3001", nil)
+}
